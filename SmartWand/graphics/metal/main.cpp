b67ed82630c7d6e963e95ab65f5f3a6d67379238
hoodude717 <156257453+hoodude717@users.noreply.github.com>
2025-08-28 16:25:49 -0600

    Adding metal implementation for macos support

---
+++ SmartWand/graphics/metal/main.cpp
@@ -0,0 +1,13 @@
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMLEMENTATION
+
+#include <Metal/Metal.hpp>
+#include <Metalkit/MetalKit.hpp>
+#include <Appkit/AppKit.hpp>
+
+
+int main(int argc, const char * argv) {
+
+}
